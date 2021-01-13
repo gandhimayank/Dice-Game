69c82afabcda58f5172ec0cf3f6e39766a442ff6
gandhimayank <63613296+gandhimayank@users.noreply.github.com>
2021-01-13 19:32:55 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const button = document.querySelector("button");
+const player1 = document.querySelector("#player1");
+const player2 = document.querySelector("#player2");
+const output = document.querySelector("#output");
+
+button.addEventListener("click",function(){
+    let rolls = [roll(6),roll(6)];
+    console.log(rolls);
+    let temp;
+    if(rolls[0] == rolls[1]) {
+        temp = "It was draw";
+    }else if (rolls[0] > rolls[1]) {
+        temp = "Player1 wins"
+    }else {
+        temp = "Player2 wins"; 
+    }
+    output.innerHTML = temp;
+    player1.innerHTML = rolls[0];
+    player2.innerHTML = rolls[1];
+})
+
+function roll(num) {
+    let rNumber = Math.floor(Math.random() * num) + 1;
+    let n = 9855 + rNumber;
+    let char = "&#"+n+";";
+
+    return rNumber + " "+ char;
+}
